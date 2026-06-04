78ce01d16936e59deb9f4add93b1814556f16f6e
vikas <vikas934776@gmail.com>
2026-06-04 13:58:52 +0530

    changed viteconfig

--- react-dashboard/vite.config.js
+++ react-dashboard/vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    server: {
-        proxy: {
-            "/api": {
-                target: import.meta.env.VITE_API_URL,
-                changeOrigin: true,
-                secure: false
-            }
-        }
-    }
+    plugins: [react()],
 })
